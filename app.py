4e7051e3b6fa7a5ad7a5674f8116e3b89c383de0
Dhanush N <71968114+RorYin@users.noreply.github.com>
2022-01-15 19:32:52 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from os import replace
 import requests
 import bs4
 from flask import Flask,jsonify,request
-import bytopic
+
 
 #Setting the flask app
 app = Flask(__name__)
@@ -11,52 +11,30 @@ app.url_map.strict_slashes=False
 url="https://www.india.com/news/world/page/"
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
+
 def getdata(query):
-       
-    switch={
-        "entertainment":"https://www.india.com/entertainment/page",
-        "topnews":"https://www.india.com/news/india/page",
-        "world":"https://www.india.com/news/world/page",
-        "business":"https://www.india.com/business/page",
-        "education":"https://www.india.com/education/page",
-        "technology":"https://www.india.com/technology/page",
-        "cricket":"https://www.india.com/cricket/page",
-        "food":"https://www.india.com/food/page",
-        "travel":"https://www.india.com/travel/page",
-        "viral":"https://www.india.com/viral/page",
-        "lifestyle":"https://www.india.com/lifestyle/page"
-    }
-    q=switch.get(query,"NA")
-    if q== "NA":
-        data=bytopic.getdata(query)
-        if data != [] :
-            return data
-    else:
-        cl="catPgListitem"
-        url=q
+    
+    url="https://www.india.com/topic/"+query+"/page/"
     output=[]
     
     
 
-    for k in range(1,6):
+    for k in range(1,4):
         try:
 
-            res = requests.get(url+f"{k}",headers).text
+            res = requests.get(url+f"{k}"+'/',headers).text
+           
         except:
-            return "Please enter a keyword or a valid topic"
-        soup = bs4.BeautifulSoup(res,'lxml')
-        i = soup.find_all('li',class_=cl)
-        print(i)
+            
+            return result
 
+        soup = bs4.BeautifulSoup(res,'lxml')
+        i = soup.find_all('li',class_="contentblk blkwrp")
+        
         for a in range(0,len(i)):
-            t=i[a]
-            p=t.findAll("p") 
-            temp=p[1].text
-            temp2=temp.replace('\n','')
-                               
-            temp3=temp2.strip()
-            headline=i[a].h3.text
-            paragraph=temp3.strip()
+            p=i[a].findAll("p")
+            headline=i[a].img['title']
+            paragraph=p[1].text
             image_url=i[a].img["data-lazy-src"]
             source_url=i[a].a['href']
             date=p[0].text
@@ -68,8 +46,12 @@ def getdata(query):
             result={"headline":headline ,"paragraph":paragraph,"image url":image_url,"source url":source_url,"date":date}
             output.append(result)
         
-    #print(output)
+    
     return output
+    
+    
+
+    
 
 
 @app.route('/')
